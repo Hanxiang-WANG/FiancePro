4bc566b8c9fe59d8c182a9bcda1827d68415e72a
Hanxiang-WANG <hanxiangw@student.unimelb.edu.au>
2024-03-12 22:46:28 +1100

    search and api(get company names)

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { ChangeEvent, MouseEvent, useState } from 'react'
 
 type Props = {}
 
 const Search : React.FC<Props> = (props: Props) : JSX.Element => {
+  const [search, setSearch] = useState<string>("");
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
   return (
-    <div>Search</div>
+    <div>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+    </div>
   )
 }
 
